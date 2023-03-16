9a15c95d403161899a3675fbb1e7e0f6ace6e9a0
nuel <nuel@gbirayon18.org>
2023-03-16 16:38:20 +0700

    fixing middlewares absensi

--- src/middlewares/absensi.middlewares.js
+++ src/middlewares/absensi.middlewares.js
@@ -6,18 +6,18 @@ const { httpStatus } = require("../variables/response.variable");
 const createAbsensi = async (req, res, next) => {
   try {
     const schema = Joi.object({
-      tanggal: Joi.string().required("Tanggal wajib diisi"),
+      tanggal: Joi.string().required("Tanggal pelayanan wajib diisi"),
       kom1: Joi.string().required("Operator komputer 1 wajib diisi"),
-      kom2: Joi.string().optional(),
-      lighting: Joi.string().optional(),
+      kom2: Joi.string().allow(""),
+      lighting: Joi.string().allow(""),
       cam1: Joi.string().required("Kameraman wajib diisi"),
-      cam2: Joi.string().optional(),
-      cam3: Joi.string().optional(),
-      switcher: Joi.string().optional(),
-      photo: Joi.string().optional(),
-      sound1: Joi.string().optional(),
-      sound2: Joi.string().optional(),
-      ir: Joi.string().optional(),
+      cam2: Joi.string().allow(""),
+      cam3: Joi.string().allow(""),
+      switcher: Joi.string().allow(""),
+      photo: Joi.string().allow(""),
+      sound1: Joi.string().required("Soundman wajib diisi"),
+      sound2: Joi.string().allow(""),
+      ir: Joi.string().allow(""),
     });
 
     const result = schema.validate({
