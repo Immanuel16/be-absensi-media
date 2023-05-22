9afdbd2c2cf067da6e3b5ffba01b84b39e0cea02
nuel <nuel@gbirayon18.org>
2023-05-22 22:15:40 +0700

    add crew list api public

--- src/routes/v1/public/general.router.js
+++ src/routes/v1/public/general.router.js
@@ -3,6 +3,7 @@ const generalController = require("../../../controllers/general.controller");
 const recruitmenController = require("../../../controllers/recruitment.controller");
 const scheduleShootingController = require("../../../controllers/schedule_shooting.controller");
 const rsvpRetreatController = require("../../../controllers/rsvp.controller");
+const userController = require("../../../controllers/user.controller");
 
 const router = express.Router();
 
@@ -22,5 +23,8 @@ router.get("/shooting/list", scheduleShootingController.getListSchedule);
 router.post("/retreat/add", rsvpRetreatController.createAvailRetreat);
 
 router.get("/retreat/list", rsvpRetreatController.getListRetreat);
+
+/* get list user */
+router.get("/crew/list", userController.getListUserAbsence);
 
 module.exports = router;
