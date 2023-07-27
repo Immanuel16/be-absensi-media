13c1b5fd8c36ab8dc76887c13ddea4400dbde13d
nuel <nuel@gbirayon18.org>
2023-07-27 16:39:55 +0700

    add routes update sapaan gembala

--- src/routes/v1/apps/index.js
+++ src/routes/v1/apps/index.js
@@ -4,22 +4,22 @@ const userProfileRoutes = require("./user_profiles.router");
 const absensiRoutes = require("./absensi.router");
 const accountsRoutes = require("./account.router");
 const historyCashRoutes = require("./riwayat_kas.router");
+const generalController = require("../../../controllers/general.controller");
 
 // import express from 'express';
 // import passport from 'passport';
 // import userProfileRoutes from "./user_profiles"
 
-require('../../../utils/passport.util');
+require("../../../utils/passport.util");
 
 const router = express.Router();
 
 /* middleware for all api has login */
-router.use(passport.authenticate('user', {session: false}));
-
-router.use('/users', userProfileRoutes);
-router.use('/absensi', absensiRoutes);
-router.use('/accounts', accountsRoutes);
-router.use('/history-cash', historyCashRoutes);
-
+router.use(passport.authenticate("user", { session: false }));
+router.put("/sapaan-gembala", generalController.updatePastorGreetings);
+router.use("/users", userProfileRoutes);
+router.use("/absensi", absensiRoutes);
+router.use("/accounts", accountsRoutes);
+router.use("/history-cash", historyCashRoutes);
 
 module.exports = router;
