4074401a9dda7c79b357627aec9572c3593bea3d
nuel <nuel@gbirayon18.org>
2023-07-27 16:27:14 +0700

    add router for update sapaan gembala

--- src/routes/v1/public/general.router.js
+++ src/routes/v1/public/general.router.js
@@ -10,6 +10,7 @@ const router = express.Router();
 router.get("/ministries-schedule", generalController.getListScheduleMinistries);
 router.get("/ministries-division", generalController.getListDivision);
 router.get("/sapaan-gembala", generalController.getPastorGreetings);
+router.put("/sapaan-gembala", generalController.updatePastorGreetings);
 
 router.post("/recruitment/add", recruitmenController.createNewCandidate);
 
