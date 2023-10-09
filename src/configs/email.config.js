4bee7b9efaa2d9518420c4a4b643a0fca63954b4
nuel <nuel@gbirayon18.org>
2023-10-09 14:48:21 +0700

    fixing email config

--- src/configs/email.config.js
+++ src/configs/email.config.js
@@ -1,8 +1,9 @@
 const config = require("./env.config");
 
 module.exports = {
+  service: "gmail",
   host: config.email.host,
   auth: config.email.auth,
   port: config.email.port,
-  secure: config.email.secure
+  secure: config.email.secure,
 };
--- src/configs/env.config.js
+++ src/configs/env.config.js
@@ -25,7 +25,7 @@ const config = {
       pass: process.env.EMAIL_PASS,
     },
     port: 587,
-    secure: true,
+    secure: false,
   },
 };
 
