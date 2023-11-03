31fe2ed6906b6355ad5349b5295e0f3b3c152edb
nuel <nuel@gbirayon18.org>
2023-11-03 15:03:25 +0700

    move api log to public

--- src/routes/v1/public/general.router.js
+++ src/routes/v1/public/general.router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const generalController = require("../../../controllers/general.controller");
-const recruitmenController = require("../../../controllers/recruitment.controller");
+const recruitmentController = require("../../../controllers/recruitment.controller");
 const scheduleShootingController = require("../../../controllers/schedule_shooting.controller");
 const rsvpRetreatController = require("../../../controllers/rsvp.controller");
 const userController = require("../../../controllers/user.controller");
 const historyCashController = require("../../../controllers/riwayat_kas.controller");
+const logErrorController = require("../../../controllers/api_log.controller");
 
 const router = express.Router();
 
@@ -13,8 +14,10 @@ router.get("/ministries-division", generalController.getListDivision);
 router.get("/sapaan-gembala", generalController.getPastorGreetings);
 router.put("/sapaan-gembala", generalController.updatePastorGreetings);
 
-router.post("/recruitment/add", recruitmenController.createNewCandidate);
-router.get("/recruitments", recruitmenController.getListCandidate);
+router.get("/log-errors", recruitmentController.getListCandidate);
+
+router.post("/recruitment/add", recruitmentController.createNewCandidate);
+router.get("/recruitments", recruitmentController.getListCandidate);
 
 router.post(
   "/shooting-request/add",
